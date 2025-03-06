cd29ef2f0834605c2ce0e2038c7681c3cc6daa55
Yorbi Mendez <yorbigmendez@gmail.com>
2025-03-06 08:08:35 -0600

    First commit

---
+++ app/ui/Fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export { inter, lusitana };
